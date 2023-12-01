85db672e603e37c8882c33578e8ae7714de78dfd
Thaaan <ethalo10@gmail.com>
2023-11-30 16:11:14 -0800

    almost done

--- ESPM50ac/assets/js/script.js
+++ ESPM50ac/assets/js/script.js
@@ -21,20 +21,17 @@ document.addEventListener('mousemove', function (e) {
 document.addEventListener("DOMContentLoaded", () => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        // Add the 'in-view' class when the element is in the viewport
         if (entry.isIntersecting) {
           entry.target.classList.add('in-view');
         } else {
-          // Optional: Remove the class if the element goes out of view
           entry.target.classList.remove('in-view');
         }
       });
     }, {
-      threshold: 0.1 // Determines how much of the item must be visible before the transition starts
+      threshold: 0.1
     });
   
-    // Observe all elements with the 'text-entry' class
-    document.querySelectorAll('.text-entry').forEach(el => {
+    document.querySelectorAll('.fade').forEach(el => {
       observer.observe(el);
     });
   });  
